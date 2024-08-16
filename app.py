b19e65d78ba4f2f12aeb28e96ed019e9169b5007
Laura Hengel <hengelaura@gmail.com>
2024-08-16 15:21:44 -0500

    navbar issues

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, )
 
 
 
@@ -9,11 +9,7 @@ app = Flask(__name__)
 def first_route():
     return render_template("index.html")
 
-@app.route('/favicon.ico') 
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route("/projects", methods=["GET"])
+@app.route("/projects", methods=["GET", "POST"])
 def projects():
     return render_template("projects.html")
  
