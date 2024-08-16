9096431eca161f0b81e08b09a6bab7f4e7afd6ec
Laura Hengel <hengelaura@gmail.com>
2024-08-16 14:46:46 -0500

    addressing favicon error

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -7,6 +8,10 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def first_route():
     return render_template("index.html")
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/projects", methods=["GET"])
 def project():
